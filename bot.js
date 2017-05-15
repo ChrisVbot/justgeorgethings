03568ac03c6eb1d45662e6b2b6c6f39ef155390c
ChrisVbot <chris.veillette@gmail.com>
2017-05-14 17:46:56 -0700

    removed function declaration

--- bot.js
+++ bot.js
@@ -12,31 +12,29 @@ const bot = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 const filePath = './static/georgeQuotes.csv';
 
-function tweetQuote() {
-    const tweetData = fs.createReadStream(filePath)
-        .pipe(csvparse({delimiter: ','}))
-        .on('data', row => {
-            inputText = inputText + ' ' + row[0];
-        })
-        .on('end', function() {
-            const markov = new rita.RiMarkov(3);
-            markov.loadText(inputText);
-            const sentence = markov.generateSentences(1);
-            bot.post('statuses/update', {status: sentence}, ((err, data, resp) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(`Status tweeted: ${sentence}`);
-                }
-            }));
-        });
-}
+const tweetData = fs.createReadStream(filePath)
+    .pipe(csvparse({delimiter: ','}))
+    .on('data', row => {
+        inputText = inputText + ' ' + row[0];
+    })
+    .on('end', function() {
+        const markov = new rita.RiMarkov(3);
+        markov.loadText(inputText);
+        const sentence = markov.generateSentences(1);
+        bot.post('statuses/update', {status: sentence}, ((err, data, resp) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Status tweeted: ${sentence}`);
+            }
+        }));
+    });
 
-tweetQuote();
+
+
 
